e416fd8c6ce23e9418c0e7577e4e96a341283350
Jordan Poles <jpoles1@gmail.com>
2015-08-18 00:53:58 -0500

    First commit

---
+++ RSS.hs
@@ -0,0 +1,34 @@
+module RSS where
+  import Network.HTTP
+  import Data.List
+  import Text.XML.Light
+  import Control.Monad
+  import Debug.Trace
+  import qualified Data.ByteString.Lazy.Char8 as L
+  -- Types containing data on individual RSS entries
+  data RssEntry = RssEntry {guid :: Maybe String, title:: Maybe String, link :: Maybe String}
+  instance Show RssEntry where
+    show x =  "Title: " ++ (show . guid $ x) ++ "\nGUID: " ++ (show . title $ x) ++ "\nLink : " ++ (show . link $ x)
+  type RssData = [RssEntry]
+  -- Types containing data on RSS feeds
+  type RssSources = [String]
+  getStories :: RssSources -> [IO RssData]
+  getStories = undefined
+  displayFeeds :: [IO RssData] -> IO String
+  displayFeeds dat = msum $ map (\x -> trace (show x) (liftM $ prettyRss x)) dat
+  fetchFeeds :: RssSources -> [IO RssData]
+  fetchFeeds srcs = map (\src -> getRssData src >>= return . parseRssData) srcs
+  getRssData :: String -> IO [Content]
+  getRssData src = do
+    xmldat <- simpleHTTP (getRequest src) >>= getResponseBody
+    return $ parseXML xmldat
+  parseRssData :: [Content] -> RssData
+  parseRssData xml = map (\(guid, title, link) -> RssEntry guid title link) entrytuples
+    where body = elChildren . head . elChildren . last . onlyElems $ xml
+          getAttr :: String -> [Maybe String]
+          getAttr attr = filter (/=Nothing) $ map ((fmap strContent) . (findChild $ items attr)) body
+          items x = QName {qName = x, qURI = Nothing, qPrefix = Nothing}
+          entrytuples = zip3 (getAttr "guid") (getAttr "title") (getAttr "link")
+  prettyRss :: RssData -> String
+  prettyRss rss = "RSS Contents" ++ div 60 ++ (concat . (intersperse $ div 15) $ map (show) rss)
+    where div n = "\n\n" ++ replicate n '-' ++ "\n\n"
